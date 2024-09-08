8970c8f9cba98a72dc0175ff8c345fce549bc6f6
evalau72 <laueva1219@gmail.com>
2024-09-08 18:21:04 -0400

    make things prettier on mobile

--- src/App.js
+++ src/App.js
@@ -66,55 +66,55 @@ function Content({ src, color, name, medium, year, isVideo }) {
 
 function App() {
   const prints = [
-    [downTheDrain, 'Green', 'Down the drain', 'linocut on paper', '2024', false],
-    [redacted, 'Red', 'Redacted', 'screenprint on paper', '2022', false],
-    [generations, 'Blue', 'Generations', 'screenprint on paper', '2022', false],
-    [threeWiseFools, 'Blue', 'Three wise fools', 'collage with screenprints and linocuts', '2022', false],
-    [kitchenShelves, 'Green', 'Banks Street kitchen - shelves', 'lithograph on paper', '2023', false],
-    [waterTiles, 'Red', 'Water tiles', 'screenprint on paper', '2022', false],
-    [hongKong, 'Green', 'Hong Kong map', 'lithograph and screenprint on paper', '2023', false],
-    [kitchenTable, 'Red', 'Banks Street kitchen - table', 'lithograph on paper', '2023', false],
-    [kittyLove, 'Blue', 'Love', 'lithograph on paper', '2023', false],
-    [lessOfAStrangerBlack, 'Blue', 'Less of a stranger (black)', 'linocut on paper', '2023', false],
-    [lessOfAStrangerGold, 'Red', 'Less of a stranger (gold)', 'linocut on paper', '2023', false],
+    [downTheDrain, 'Green', '"Down the drain"', 'linocut on paper', '2024', false],
+    [redacted, 'Red', '"Redacted"', 'screenprint on paper', '2022', false],
+    [generations, 'Blue', '"Taxonomy"', 'screenprint on paper', '2022', false],
+    [threeWiseFools, 'Blue', '"Three wise fools"', 'collage with screenprints and linocuts', '2022', false],
+    [kitchenShelves, 'Green', '"Banks Street kitchen - shelves"', 'lithograph on paper', '2023', false],
+    [kitchenTable, 'Red', '"Banks Street kitchen - table"', 'lithograph on paper', '2023', false],
+    [waterTiles, 'Red', '"Water tiles"', 'screenprint on paper', '2022', false],
+    [hongKong, 'Green', '"Hong Kong map"', 'lithograph and screenprint on paper', '2023', false],
+    [kittyLove, 'Blue', '"Love"', 'lithograph on paper', '2023', false],
+    [lessOfAStrangerBlack, 'Blue', '"Less of a stranger (black)"', 'linocut on paper', '2023', false],
+    [lessOfAStrangerGold, 'Red', '"Less of a stranger (gold)"', 'linocut on paper', '2023', false],
   ]
 
   const paintings = [
-    [glassWater, 'Red', 'Glass and water', 'acrylic on canvas', '2024', false],
-    [home, 'Green', 'Home', 'oil on canvas', '2021', false],
-    [atoll, 'Red', 'Atoll', 'acrylic on paper', '2022', false],
-    [flowerPeople, 'Blue', 'Flower people', 'acrylic and wire on paper', '2021', false],
-    [bathroom, 'Red', 'Bathroom', 'gouache on paper', '2021', false],
-    [ferrisWheel, 'Green', 'Ferris wheel', 'oil on canvas', '2021', false],
-    [familyDinner, 'Blue', 'Family dinner', 'oil on canvas', '2021', false],
-    [mindPalace, 'Green', 'Mind palace', 'oil on canvas', '2021', false],
+    [glassWater, 'Red', '"Glass and water"', 'acrylic on canvas', '2024', false],
+    [home, 'Green', '"Home"', 'oil on canvas', '2021', false],
+    [atoll, 'Red', '"Atoll"', 'acrylic on paper', '2022', false],
+    [flowerPeople, 'Blue', '"Flower people"', 'acrylic and wire on paper', '2021', false],
+    [bathroom, 'Red', '"Bathroom"', 'gouache on paper', '2021', false],
+    [ferrisWheel, 'Green', '"Ferris wheel"', 'oil on canvas', '2021', false],
+    [familyDinner, 'Blue', '"Family dinner"', 'oil on canvas', '2021', false],
+    [mindPalace, 'Green', '"Mind palace"', 'oil on canvas', '2021', false],
   ]
 
   const homeComingExhibition = [
     [exhibitionPoster, 'Green', 'Exhibition poster', false],
     [artistStatement, 'Red', "Artist's statement", false],
     [exhibition, 'Blue', "Exhibition room", false],
-    [somp1, 'Red', 'Shadow of my past (part 1)', 'lasercut on paper', '2023', false],
-    [banksStreetKitchen, 'Blue', "Banks street kitchen", 'lithograph on paper and cling wrap', '2023', false],
-    [somp2, 'Red', 'Shadow of my past (part 2)', 'lasercut on paper', '2023', false],
-    [home, 'Green', 'Home', 'oil on canvas', '2021', false],
-    [threeWiseFools, 'Blue', 'Three wise fools', 'collage with screenprints and linocuts', '2022', false],
-    [dragonMap, 'Red', 'Dragon map', 'embroidery on canvas bag', '2022', false],
-    [lessOfAStranger, 'Blue', 'Less of a stranger', 'linocut on paper', '2023', false],
-    [somp3, 'Red', 'Shadow of my past (part 3)', 'lasercut on paper', '2023', false],
-    [redacted, 'Green', 'Redacted', 'screenprint on paper', '2022', false],
-    [familyDinner, 'Blue', 'Family dinner', 'oil on canvas', '2021', false],
-    [somp4, 'Red', 'Shadow of my past (part 4)', 'lasercut on paper', '2023', false],
-    [atoll, 'Red', 'Atoll', 'acrylic on paper', '2022', false],
-    [atollCharcoal, 'Red', 'Atoll', 'charcoal on paper', '2022', false],
-    [blueprint, 'Green', 'Blueprint', 'paper stencil', '2023', false],
-    [blueprintFront, 'Green', 'Blueprint', 'paper stencil', '2023', false],
+    [somp1, 'Red', '"Shadow of my past (part 1)"', 'lasercut on paper', '2023', false],
+    [banksStreetKitchen, 'Blue', '"Banks street kitchen"', 'lithograph on paper and cling wrap', '2023', false],
+    [somp2, 'Red', '"Shadow of my past (part 2)"', 'lasercut on paper', '2023', false],
+    [home, 'Green', '"Home"', 'oil on canvas', '2021', false],
+    [threeWiseFools, 'Blue', '"Three wise fools"', 'collage with screenprints and linocuts', '2022', false],
+    [dragonMap, 'Red', '"Dragon map"', 'embroidery on canvas bag', '2022', false],
+    [lessOfAStranger, 'Blue', '"Less of a stranger"', 'linocut on paper', '2023', false],
+    [somp3, 'Red', '"Shadow of my past (part 3)"', 'lasercut on paper', '2023', false],
+    [redacted, 'Green', '"Redacted"', 'screenprint on paper', '2022', false],
+    [familyDinner, 'Blue', '"Family dinner"', 'oil on canvas', '2021', false],
+    [somp4, 'Red', '"Shadow of my past (part 4)"', 'lasercut on paper', '2023', false],
+    [atoll, 'Red', '"Atoll"', 'acrylic on paper', '2022', false],
+    [atollCharcoal, 'Red', '"Atoll"', 'charcoal on paper', '2022', false],
+    [blueprint, 'Green', '"Blueprint"', 'paper stencil', '2023', false],
+    [blueprintFront, 'Green', '"Blueprint"', 'paper stencil', '2023', false],
   ]
 
   const sections = [
     [prints, "PRINTS"],
     [paintings, "PAINTINGS"],
-    [homeComingExhibition, "HOME COMING EXHIBITION"]
+    [homeComingExhibition, '"HOME COMING" EXHIBITION']
   ]
 
   return (
@@ -123,15 +123,18 @@ function App() {
         <img src={logo} className="logo"/>
         <div className="header">
           <span className="bio">
-            Eva Lau is an artist (and occasional engineer) in nyc.
+            Eva Lau is a mixed media artist that loves to make prints and is also an occasional programmer.
           </span>
-          <i> Mainly, however, she seems to just enjoy making things that make her happy.</i>
+          <span className='subBioContainer'>
+            <i className='subBio'> based in nyc, inspired by home</i>
+            <i className='subBio'> (hong kong, bos, pvd)</i>
+          </span>
         </div>
       </div>
       {/* <div>
        <p className="warning">this site is under construction, but it welcomes all visitors :)</p>
       </div> */}
-      <div>
+      <div className='sections'>
         {sections.map(([section, title]) => (
           <div className='section'>
             <span className='sectionTitle'>{title}</span>  
